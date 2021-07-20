06cd67924090d134803880bce048b61cdf08ae49
wangbo <98wangbo>
2021-07-20 14:22:12 +0800

    route重构

--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import routeReact from './route-react'
-import routeList from './route-list'
-const InitRoute = () => {
-    const routes = routeList.apps.concat(routeList.others)
-    return (
-       routeReact(routes)
-    )
-}
-export default InitRoute
