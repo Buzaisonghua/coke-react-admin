7f8e29a1db21058f736b4091a7595521120f455b
wangbo <98wangbo>
2021-07-20 14:28:50 +0800

    route-component 创建

---
+++ src/route/route-component.ts
@@ -0,0 +1,11 @@
+import Login from '@/views/login'
+import Layout from '@/layout'
+import Home from '@/views/home'
+import User from '@/views/user'
+
+export default {
+    Login,
+    Layout,
+    Home,
+    User
+}
